20175b5aa16880846ea55b609f86a4738f3ed250
Neil Singh <singhn1@ewsdmail.org>
2024-12-22 12:51:08 -0500

    Swerve base code

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Reefscape-feat-setpoint-generator";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "7b61823af8c90046df27f20aa3cf7cc081f5f633";
+  public static final String GIT_DATE = "2024-12-15 15:09:34 EST";
+  public static final String GIT_BRANCH = "Auto-Aim-Swerve";
+  public static final String BUILD_DATE = "2024-12-20 09:21:35 EST";
+  public static final long BUILD_UNIX_TIME = 1734704495754L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
